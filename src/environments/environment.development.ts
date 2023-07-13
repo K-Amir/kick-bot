965d89b9b154f6a4b68f63664d6eaf2815a3426d
khlvamir <khlvamir@gmail.com>
2023-07-13 22:18:55 +0200

    List with endpoints

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  kickV1: 'https://kick.com/api/v1/',
-  kickV2: 'https://kick.com/api/v2/',
+  kickV1: 'https://kick.com/api/v1',
+  kickV2: 'https://kick.com/api/v2',
 };
